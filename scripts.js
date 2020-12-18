b21c5f55a130f37fcc7c85debd4d4296b86d5150
Jill-Ann <jillannkay@gmail.com>
2020-12-18 17:50:51 +0100

    Tidy up code

--- scripts.js
+++ scripts.js
@@ -1,45 +1,45 @@
 const main = document.getElementById("root");
-const container = document.createElement("div"); /*creates new div*/
+const container = document.createElement("div");
 container.setAttribute("class", "container");
 
 main.appendChild(container);
 
 const useFetchApi = () => {
   fetch("https://randomuser.me/api/?results=10")
-  .then(response => response.json())
-  .then(data => {
-    data.results.forEach(result => {
-      // Create a div with a card class
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((result) => {
+        // Create a div with a card class
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
 
-      // Create an h2 and set the text content to name
-      const h2 = document.createElement("h2");
-      // h2.textContent = result.name;
-      h2.textContent = `${result.name.first} ${result.name.last}`;
+        // Create an h2 and set the text content to name
+        const h2 = document.createElement("h2");
 
-      const p = document.createElement("p");
-      p.textContent = `${result.location.city}, ${result.location.state}`;
+        h2.textContent = `${result.name.first} ${result.name.last}`;
 
-      const img = document.createElement("img");
-      img.src = result.picture.large;
+        const p = document.createElement("p");
+        p.textContent = `${result.location.city}, ${result.location.state}`;
 
-      // Append the cards to the container element
-      container.appendChild(card);
-      card.appendChild(h2);
-      card.appendChild(p);
-      card.appendChild(img);
+        const img = document.createElement("img");
+        img.src = result.picture.large;
+
+        // Append the cards to the container element
+        container.appendChild(card);
+        card.appendChild(h2);
+        card.appendChild(p);
+        card.appendChild(img);
+      });
     });
-  })
-}
+};
 
 const usejQuery = () => {
-  $(document).ready(function() {
+  $(document).ready(function () {
     $.ajax({
       url: "https://randomuser.me/api/?results=10",
       dataType: "json",
-      success: function(data) {
-        data.results.forEach(result => {
+      success: function (data) {
+        data.results.forEach((result) => {
           // Create a div with a card class
           const card = document.createElement("div");
           card.setAttribute("class", "card");
@@ -62,10 +62,10 @@ const usejQuery = () => {
           card.appendChild(p);
           card.appendChild(img);
         });
-      }
+      },
     });
   });
-}
+};
 
 $("#fetch").click(useFetchApi);
 $("#jquery").click(usejQuery);
